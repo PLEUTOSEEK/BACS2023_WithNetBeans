78f758e4b4a6c3fd3dc1d86635d0966b9db68471
Tee Zhuo Xuan <xuanvanquish@gmail.com>
2021-06-19 17:15:00 +0800

    GOGOSUBMITNETBEANS

--- BACS2023/src/bacs2023/BACS2023.java
+++ BACS2023/src/bacs2023/BACS2023.java
@@ -17,6 +17,7 @@ public class BACS2023 {
     public static void main(String[] args) {
         // TODO code application logic here
         //some code here...
+        //try commit
     }
     
 }
